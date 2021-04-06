5e6e58789cde55ac2b79bffc25f37e5bcdd3edcf
TomasCarlson <34401946+TomasCarlson@users.noreply.github.com>
2021-04-06 17:03:19 -0400

    Added links

--- src/pages/clients.js
+++ src/pages/clients.js
@@ -28,13 +28,27 @@ function ClientsPage(props) {
       <SmallHeader></SmallHeader>
       <Col sm={9} dismissible variant="danger">
         <div className="main-content d-flex flex-row flex-wrap align-content-center justify-content-center">
-          <Logoicon link={who} ></Logoicon>
-          <Logoicon link={varfaj}></Logoicon>
-          <Logoicon link={he}></Logoicon>
-          <Logoicon link={rough}></Logoicon>
-          <Logoicon link={wrbb}></Logoicon>
-          <Logoicon link={maker}></Logoicon>
-          <Logoicon link={arati}></Logoicon>
+          <a href="https://www.who.int/" target="_blank" rel="noopener noreferrer">
+            <Logoicon link={who} ></Logoicon>
+          </a>
+          <a href="https://varfaj.com/" target="_blank" rel="noopener noreferrer">
+            <Logoicon link={varfaj}></Logoicon>
+          </a>
+          <a href="https://happyendings917.com/" target="_blank" rel="noopener noreferrer">
+            <Logoicon link={he}></Logoicon>
+          </a>
+          <a href="https://roughsimmons.jp/" target="_blank" rel="noopener noreferrer">
+            <Logoicon link={rough}></Logoicon>
+          </a>
+          <a href="http://wrbbradio.org/" target="_blank" rel="noopener noreferrer">
+            <Logoicon link={wrbb}></Logoicon>
+          </a>
+          <a href="https://themakegroup.com/" target="_blank" rel="noopener noreferrer">
+            <Logoicon link={maker}></Logoicon>
+          </a>
+          <a href="https://www.aratikreibich.com/" target="_blank" rel="noopener noreferrer">
+            <Logoicon link={arati}></Logoicon>
+          </a>
         </div>
       </Col>
     </Row>
